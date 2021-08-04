2667036f4f777efe8da53fb40c8d29c6b2033d78
chihana-0831 <chihana_kashiwabara@rudel.jp>
2021-08-04 18:37:10 +0900

    add: header, logo, css

--- _parts/header.php
+++ _parts/header.php
@@ -2,7 +2,15 @@
 <html lang="ja">
 <head>
   <meta charset="utf-8">
-  <!-- <link rel="stylesheet" href="style.css"> -->
+  <link rel=“stylesheet” href=“common.css”>
   <title>New Form</title>
 </head>
 <body>
+
+<header>
+    <img src="../images/html.png" alt="logo" class="logo"> 
+    <a href="#">Post</a>
+    <a href="#">Matching</a>
+    <a href="#">Classes</a>
+    <a href="#">My Page</a>
+</header>
