f4da06ac4732e2cd4feffd714d56b6fefc82868c
chihana-0831 <chihana_kashiwabara@rudel.jp>
2021-08-05 12:04:00 +0900

    add: css to header

--- _parts/header.php
+++ _parts/header.php
@@ -3,28 +3,15 @@
 <head>
   <meta charset="utf-8">
   <title>New Form</title>
-  <link rel=“stylesheet” href="style.css">
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 <header>
     <img src="../images/html.png" alt="logo" class="logo"> 
-    <a href="#">Post</a>
-    <a href="#">Matching</a>
-    <a href="#">Classes</a>
-    <a href="#">My Page</a>
+    <a href="#"><span>Post</span></a>
+    <a href="#"><span>Matching</span></a>
+    <a href="#"><span>Classes</span></a>
+    <a href="#"><span>My Page</span></a>
 </header>
 
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html> -->
