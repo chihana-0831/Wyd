067f3c1aa087731167b5df4a30330f1d00f634dd
chihana-0831 <chihana_kashiwabara@rudel.jp>
2021-08-06 11:27:51 +0900

    add: Tell us... & css

---
+++ _body/tellus.php
@@ -0,0 +1,38 @@
+<?php
+
+require('../_parts/header.php');
+
+?>
+
+
+<div class="tellus_form">
+    <h3>Tell us about yourself</h3>
+    <form action="" method="post">
+        <label for="advisor">Advisor : </label>
+        <select name="advisor" required>
+            <option value="teacher1">teacher1</option>
+            <option value="teacher2">teacher2</option>
+        </select><br>
+        <label for ="club">Club : </label>
+        <select name="club" required>
+            <option value="club1">club1</option>
+            <option value="club2">club2</option>
+        </select><br>
+        <label for="v/jv">Versity/JV : </label>
+        <select name="v/jv" required>
+            <option value="sport1">sport1</option>
+            <option value="sport2">sport2</option>
+        </select><br>
+        <label for="interests">Interests/Hobbies : </label>
+        <select name="interests" required>
+            <option value="interest1">interest1</option>
+            <option value="interest2">interest2</option>
+        </select><br>
+        <input type="submit" class="submit" value="Submit">
+    </form>
+</div>
+
+
+<?php
+
+include('../_parts/footer.php');
