07bb9f4db01e04081b705c53e6494c5ffad5ba1b
chihana-0831 <chihana_kashiwabara@rudel.jp>
2021-08-06 18:41:36 +0900

    add: schedule page & css

---
+++ _body/schedule.php
@@ -0,0 +1,144 @@
+<?php
+
+require('../_parts/header.php');
+
+?>
+
+<div class="schedule_header">
+    <form action="" method="post">
+        <input type="submit" class="schedule_schedule" value="Schedule">
+        <p>/</p>
+        <input type="submit" class="schedule_profile" value="Profile">
+    </form>
+</div>
+
+<div class="schedule_form">
+    <table class="table_schedule">
+        <thead>
+            <tr>
+                <th>Time</th>
+                <th class="period">Period</th>
+                <th>Mon</th>
+                <th>Tue</th>
+                <th>Wed</th>
+                <th>Thu</th>
+                <th>Fri</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>8:21-9:05</th>
+                <th class="period">1</th>
+                <td>Science</td>
+                <td>Science</td>
+                <td>IP</td>
+                <td>Science</td>
+                <td>Science</td>
+            </tr>
+            <tr>
+                <th>9:06-9:26</th>
+                <th class="period">2</th>
+                <td>HS Advisory</td>
+                <td>HS Advisory</td>
+                <td>IP</td>
+                <td>HS Advisory</td>
+                <td>HS Advisory</td>
+            </tr>
+            <tr>
+                <th>9:27-10:11</th>
+                <th class="period">3</th>
+                <td>English</td>
+                <td>English</td>
+                <td>IP</td>
+                <td>English</td>
+                <td>English</td>
+            </tr>
+            <tr>
+                <th>10:12-10:32</th>
+                <th class="period">4</th>
+                <td>Morning Break</td>
+                <td>Morning Break</td>
+                <td>IP</td>
+                <td>Morning Break</td>
+                <td>Morning Break</td>
+            </tr>
+            <tr>
+                <th>10:33-11:17</th>
+                <th class="period">5</th>
+                <td>SAT Prep</td>
+                <td>SAT Prep</td>
+                <td>Social Emotional</td>
+                <td>Integrated Arts</td>
+                <td>Integrated Arts</td>
+            </tr>
+            <tr>
+                <th>11:18-12:02</th>
+                <th class="period">6</th>
+                <td>History</td>
+                <td>History</td>
+                <td>Town Meeting</td>
+                <td>History</td>
+                <td>History</td>
+            </tr>
+            <tr>
+                <th>12:03-12:43</th>
+                <th class="period">7</th>
+                <td>Lunch</td>
+                <td>Lunch</td>
+                <td>Lunch</td>
+                <td>Lunch</td>
+                <td>Lunch</td>
+            </tr>
+            <tr>
+                <th>12:44-1:24</th>
+                <th class="period">8</th>
+                <td>Creative Code</td>
+                <td>Creative Code</td>
+                <td>Creative Code</td>
+                <td>Creative Code</td>
+                <td>Creative Code</td>
+            </tr>
+            <tr>
+                <th>1:25-2:05</th>
+                <th class="period">9</th>
+                <td>AP Calc BC</td>
+                <td>AP Calc BC</td>
+                <td>AP Calc BC</td>
+                <td>AP Calc BC</td>
+                <td>AP Calc BC</td>
+            </tr>
+            <tr>
+                <th>2:05-2:25</th>
+                <th class="period">10</th>
+                <td>Mask Break</td>
+                <td>Mask Break</td>
+                <td>Mask Break</td>
+                <td>Mask Break</td>
+                <td>Mask Break</td>
+            </tr>
+            <tr>
+                <th>2:25-3:05</th>
+                <th class="period">11</th>
+                <td>AP CS</td>
+                <td>AP CS</td>
+                <td>AP CS</td>
+                <td>AP CS</td>
+                <td>AP CS</td>
+            </tr>
+            <tr>
+                <th>3:05-3:45</th>
+                <th class="period">12</th>
+                <td>Badminton</td>
+                <td>Badminton</td>
+                <td>Badminton</td>
+                <td>Badminton</td>
+                <td>Badminton</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+<?php
+
+include('../_parts/footer.php');
